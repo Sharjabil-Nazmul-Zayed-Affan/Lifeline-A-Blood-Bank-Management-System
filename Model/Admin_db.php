90c24eb3f2f208d4127ef568690a8a303cc0e0d5
Sharjabil-Nazmul-Zayed-Affan <152841121+Sharjabil-Nazmul-Zayed-Affan@users.noreply.github.com>
2026-09-03 12:38:06 +0600

    Update Admin_db.php

--- Model/Admin_db.php
+++ Model/Admin_db.php
@@ -2,7 +2,7 @@
 $host     = "localhost";
 $user     = "root";
 $password = "";
-$dbname   = " lifeline: a blood bank mangement system";
+$dbname   = "lifeline: a blood bank mangement system";
 
 $conn = mysqli_connect($host, $user, $password, $dbname);
 
